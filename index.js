305956d5c60273e9e5c2274b27ec642666766f15
Dr. Rich Cordero <rich@corderophilosophy.com>
2018-03-05 11:22:30 -0500

    fix error handling?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk')
-const {precip_threshold, temp_threshold} = require('./config')
+const { precip_threshold, temp_threshold } = require('./config')
 
 const argv = require('minimist')(process.argv.slice(2))
 const location = argv._[0]
@@ -17,9 +17,23 @@ const SUMMARY = chalk.green.bold
 
 getGeoCode(location)
   .then(getWeather)
-  .then(res => console.log(`
-    - Right now it's: ${WET(res.summary)} - 
-    - Current Actual Temp (F): ${res.temperature > temp_threshold ? HOT(res.temperature) : COLD(res.temperature)} - 
-    - Real Feel Temp (F): ${res.apparentTemperature > temp_threshold ? HOT(res.apparentTemperature) : COLD(res.apparentTemperature)} - 
-    - Precipitation Chance (%): ${res.precipProbability > precip_threshold ? WET(res.precipProbability) : DRY(res.precipProbability)} - 
-  `))
+  .then(res =>
+    console.log(`
+    - Right now it's: ${WET(res.summary)} -
+    - Current Actual Temp (F): ${
+      res.temperature > temp_threshold
+        ? HOT(res.temperature)
+        : COLD(res.temperature)
+    } -
+    - Real Feel Temp (F): ${
+      res.apparentTemperature > temp_threshold
+        ? HOT(res.apparentTemperature)
+        : COLD(res.apparentTemperature)
+    } -
+    - Precipitation Chance (%): ${
+      res.precipProbability > precip_threshold
+        ? WET(res.precipProbability)
+        : DRY(res.precipProbability)
+    } -
+  `)
+  )
